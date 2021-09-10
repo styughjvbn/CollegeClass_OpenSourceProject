fcf02e5a82f93acce57f0790bcc96ce24524cd70
styughjvbn <3338761@naver.com>
2021-09-10 17:19:50 +0900

    init

---
+++ src/example/jv11.java
@@ -0,0 +1,19 @@
+package example;
+import java.util.Scanner;
+
+public class jv11 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("수를 입력하시오: ");
+		int number=in.nextInt();
+		if(number%3==0)
+			System.out.println("3의 배수입니다.");
+		else
+			System.out.println("3의 배수가 아닙니다.");
+		
+		in.close();
+
+	}
+
+}
