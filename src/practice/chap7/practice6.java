c9c7decd5bb5c67e378d3a79c9fdd21e53cf0b10
styughjvbn <3338761@naver.com>
2021-10-28 17:13:16 +0900

    7장 예제 및 실습문제

---
+++ src/practice/chap7/practice6.java
@@ -0,0 +1,70 @@
+package practice.chap7;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+class Location{
+	String city;
+	int longitude;
+	int latitude;
+	
+	Location(String str1,int str2,int str3){
+		city=str1;
+		longitude=str2;
+		latitude=str3;
+	}
+	String get_city() {
+		return city;
+	}
+	int get_longitude() {
+		return longitude;
+	}
+	int get_latitude() {
+		return latitude;
+	}
+	void find_location(String city) {
+		System.out.println(city+"  "+longitude+"  "+latitude);
+	}
+}
+public class practice6 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Location[] location = new Location[4];
+		HashMap<String, Location> manage = new HashMap<String, Location>();
+		System.out.println("도시, 경도, 위도를 입력하세요.");
+		for(int i=0; i<location.length; i++) {
+			System.out.print(">> ");
+			String text = sc.nextLine();
+			StringTokenizer st = new StringTokenizer(text, ",");
+			String city = st.nextToken().trim();
+			int longitude = Integer.parseInt(st.nextToken().trim());
+			int latitude = Integer.parseInt(st.nextToken().trim());
+			location[i] = new Location(city, longitude, latitude);
+			manage.put(city, location[i]); // ArrayList에 저장
+		}
+		Set<String> key = manage.keySet();
+		Iterator<String> it = key.iterator();
+		System.out.println("----------------------------------");
+		while(it.hasNext()) {
+			String city = it.next();
+			Location s = manage.get(city);
+			System.out.println(s.get_city() +" "+s.get_longitude()+" "+s.get_latitude());
+		}
+		System.out.println("----------------------------------");
+		while(true) {
+			System.out.print("도시 이름 >> ");
+			String city = sc.next();
+			if(city.equals("그만"))
+				break;
+			Location s = manage.get(city);
+			if( s == null)
+				System.out.println(city+"는 없습니다.");
+			else
+				s.find_location(city);
+		}
+		sc.close();
+	}
+}
